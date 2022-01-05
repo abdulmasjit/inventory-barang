d6c3183de5a85cc8fbc7e5f8c99559333e770b35
Abdul Masjit <abdulmasjits@gmail.com>
2022-01-05 13:00:42 +0700

    setting https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +26,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        } 
+
         // Paginator::useBootstrap();
         Paginator::defaultView('layouts.pagination');
         // Currency
