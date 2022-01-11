f573d36e7ee643ffdb7df4192fada5a8b20bd098
Abdul Masjit <abdulmasjits@gmail.com>
2022-01-11 08:23:28 +0700

    Setting Prod

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -70,7 +70,5 @@
   <script src="{{ asset('assets/js/off-canvas.js') }}"></script>
   <script src="{{ asset('assets/js/hoverable-collapse.js') }}"></script>
   <script src="{{ asset('assets/js/template.js') }}"></script>
-  <script src="{{ asset('assets/js/settings.js') }}"></script>
-  <script src="{{ asset('assets/js/todolist.js') }}"></script>
 </body>
 </html>
